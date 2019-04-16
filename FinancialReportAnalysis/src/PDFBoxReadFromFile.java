dc02ee1202fa3eb6a9fe528201fd6a224c91fa04
Angela Wen <wena@seas.upenn.edu>
2019-04-16 17:35:20 +0800

    Allow PDF to text to take any file name and print .txt file with same name

--- FinancialReportAnalysis/src/PDFBoxReadFromFile.java
+++ FinancialReportAnalysis/src/PDFBoxReadFromFile.java
@@ -12,20 +12,53 @@ import java.util.logging.Logger;
  */
 public class PDFBoxReadFromFile {
 	PDFManager pdfManager = new PDFManager();
+	String fileName;
 
+	/**
+	 * PDFBoxReadFromFile constructor sets the fileName input.
+	 * @param fileName
+	 * @throws IOException
+	 */
 	public PDFBoxReadFromFile(String fileName) throws IOException {
+		this.fileName = fileName;
 		pdfManager.setFilePath(fileName); // Constructs PDF Manager with correct file name.
 	}
 	
+	/**
+	 * getString returns the String output of the PDF
+	 * @return the String output of the PDF
+	 * @throws IOException
+	 */
 	public String getString() throws IOException {
 		return pdfManager.toText();
 	}
+	
+	/**
+	 * createTxtName removes the .pdf extension of the PDF being read and
+	 * returns the .txt name of the file to write to
+	 * @return the .txt name of the file to write to
+	 */
+	private String createTxtName() {
+		// Assume input file ends in .pdf
+		
+		// Get substring of PDF file without .pdf
+		String outputFileName = fileName.substring(0, fileName.lastIndexOf("."));
+		
+		// Append _converted.txt
+		outputFileName += "_converted.txt";
+		
+		return outputFileName;
+	}
 
+	/**
+	 * printToTxt writes the String output of the PDF to the new .txt file
+	 * @throws IOException
+	 */
 	public void printToTxt() throws IOException {
 		String text = pdfManager.toText();// Retrieves String from PDF Manager
 
 		// Print to file
-		File out = new File("201409-text.txt");
+		File out = new File(createTxtName());
 
 		PrintWriter pw = new PrintWriter(out);
 		pw.print(text);
