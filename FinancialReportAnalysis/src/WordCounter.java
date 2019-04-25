08a9948550b7bb0ac0300c6905b24a12d66c966f
tculpan <tculpan@seas.upenn.edu>
2019-04-25 18:59:19 +0800

    Updates WordCounter (note: error)
    Note, this still an error at Line 40: The constructor Scanner(FileReader) is undefined

--- FinancialReportAnalysis/src/WordCounter.java
+++ FinancialReportAnalysis/src/WordCounter.java
@@ -1,52 +1,49 @@
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
+//25 April 2019: 18:56
+//Note error Line 40: The constructor Scanner(FileReader) is undefined
+
 /**
- * a WordCounter class to grab a file, then count how many occurrences of each
- * word
- * 
+ * a WordCounter class to grab a file, then count how many occurrences of each word
+ *  
  * @author Tim Culpan
  * 
  */
 
-
- 
-//
-
-public static void main(String[] args) {
-	try {
-		PDFBoxReadFromFile PDFReader = new PDFBoxReadFromFile("201409.pdf");
-		PDFReader.printToTxt();
-	//
-		
-	}//end try
-	catch (IOException e) {
-		e.printStackTrace();
-	}//end catch
-}//end main	
-
-//
-
-
 public class WordCounter {
-
+	String filename;
 	HashMap<String, Integer> wordCount = new HashMap<>();
-	String incoming;
-	Scanner input = new Scanner(incoming); 
+	WordCounter incoming;
+	
 	String currentWord;
+	 
+	/** CONSTRUCTOR 
+	 * @param fileName
+	 *  @throws IOException
+	 */	
+	
+	public WordCounter (String filename) throws IOException {
+		this.filename = filename;
+	}
 
 	/**
+	 * This method creates a HashMap of words and no. of occurrences
 	 * 
 	 * @param filename the incoming file we want to analyze
-	 * @return HashMap of each word and how many times it occurs
+	 * @return HashMap (word, occurrences)
 	 * 
 	 */		
 
 	public HashMap<String, Integer> countOfWords(String filename) {
-		incoming = filename;
+		this.filename = filename;
+		FileReader countThis = new FileReader(filename);
+		ArrayList<String> fileAsArray = countThis.getfileReadIn();
+				
+		Scanner inbound = new Scanner(countThis); 
 
-		while (input.hasNext()) {
-			String currentWord = input.next();
+		while (inbound.hasNext()) {
+			String currentWord = inbound.next(); 
 
 			if (wordCount.containsKey(currentWord)) {
 				wordCount.put(currentWord, wordCount.get(currentWord) + 1);
@@ -62,5 +59,30 @@ public class WordCounter {
 		
 	}// END countOfWords method
 	
+	
+	
+	//This main method is just to implement the WordCounter.
+	// can be removed and folded into our Runner class
+	public static void main (String[] args) {
+		
+			try {
+				HashMap<String, Integer> count = new HashMap<>();
+				
+				// Actually, I think we don't need this
+				WordCounter counter = new WordCounter("201409_converted.txt");
+				
+				count = counter.countOfWords("201409_converted.txt");
+					    		
+	    		System.out.println(count);
+	    		
+	    		//
+	    	
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			 
+	 }
+
 
 }// END WordCounter Class
