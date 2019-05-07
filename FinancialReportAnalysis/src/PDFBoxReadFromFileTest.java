70511cef180e48bd7c77125230a2d042b04a77c3
Angela Wen <wena@seas.upenn.edu>
2019-05-07 17:07:00 +0800

    Fix JUnit test with file name

--- FinancialReportAnalysis/src/PDFBoxReadFromFileTest.java
+++ FinancialReportAnalysis/src/PDFBoxReadFromFileTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,8 @@ class PDFBoxReadFromFileTest {
 	@Test
 	void test() {
 		try {
-    		PDFBoxReadFromFile PDFReader = new PDFBoxReadFromFile("201409.pdf");
+    		File f = new File("pdf/BABA201409.pdf");
+			PDFBoxReadFromFile PDFReader = new PDFBoxReadFromFile(f);
     		assertTrue(PDFReader.getString().contains("Alibaba Group") && 
     				PDFReader.getString().contains("September 30, 2014"));
 		} catch(IOException e) {
