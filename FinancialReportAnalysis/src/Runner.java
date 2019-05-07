dfd8adbf9fc13e984a06fe9eb792023814f3df45
Angela Wen <wena@seas.upenn.edu>
2019-05-07 17:56:33 +0800

    Refactor Runner class with instance variables and constructor

--- FinancialReportAnalysis/src/Runner.java
+++ FinancialReportAnalysis/src/Runner.java
@@ -1,82 +1,121 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-/** The program utilize two external libraries PDF Box and SentimentAnalysis to read from PDFs 
- * and conduct sentiment analysis on financial statements
+import java.util.Map;
+
+/**
+ * The program utilize two external libraries PDF Box and SentimentAnalysis to
+ * read from PDFs and conduct sentiment analysis on financial statements
  * 
- * @author Cryrus
+ * @author Cryrus Cheng, Angela Wen
  *
- *Credit to  Ruthwik at 
- * https://github.com/Ruthwik/Sentiment-Analysis
+ *         Credit to Ruthwik at https://github.com/Ruthwik/Sentiment-Analysis
  * 
- * Credit to RadixCode at
- * https://radixcode.com/pdfbox-example-code-how-to-extract-text-from-pdf-file-with-java
- * 
- * The idea is to loop through all the pdf files.
- * 1) 
+ *         Credit to RadixCode at
+ *         https://radixcode.com/pdfbox-example-code-how-to-extract-text-from-pdf-file-with-java
  * 
  */
 public class Runner {
 
-	public static void main(String[] args) throws IOException {
-		
-		HashMap<String, FinancialData> finDataHM = new HashMap<String, FinancialData>();
-		HashMap<String, SentimentAnalysisResult> senResultHM = new HashMap<String, SentimentAnalysisResult>();
-		
-		//Loop 1, loop through all pdfs in a folder and export to txt files in folder "txt"
+	Map<String, FinancialData> finDataHM = new HashMap<String, FinancialData>();
+	Map<String, SentimentAnalysisResult> senResultHM = new HashMap<String, SentimentAnalysisResult>(); // object
+	Map<String, Integer> wordCountHM = new HashMap<String, Integer>(); // Maps word:occurrence
+
+	/**
+	 * The Runner constructor loops through all PDFs in a folder and exports them to
+	 * .txt
+	 */
+	public Runner() throws IOException {
+		// Loop through all PDFs in a folder and export to .txt files in folder "txt"
+		PDFtoTxt();
+
+		// Loop through all txt files to fill hashmaps
+		readTxts();
+	}
+
+	/**
+	 * PDFtoTxt opens all PDFs within the "pdf" folder and creates a txt file with
+	 * PDFBoxReadFromFile.
+	 * 
+	 * @throws IOException
+	 */
+	private void PDFtoTxt() throws IOException {
 		File pdfFolder = new File("pdf");
 		for (File file : pdfFolder.listFiles(new PDFFileFilter())) {
-			
 			PDFBoxReadFromFile PDFReader = new PDFBoxReadFromFile(file);
-			PDFReader.printToTxt();  //Generate a txt file "filename_converted.txt" for use of DataParser
+			PDFReader.printToTxt(); // Generate a txt file "filename_converted.txt" for use of DataParser
 		}
-		
-		//Loop2, for the same company, use the same parser to loop through all quarters
+	}
+
+	/**
+	 * readTxts opens all txt files, adds to "txt" folder, and initializes
+	 * FinancialData and SentimentAnalysis objects after parsing with DataParser. It
+	 * then inputs the FinancialData and SentimentAnalysis objects into the
+	 * corresponding hashmap.
+	 */
+	private void readTxts() {
 		File txtFolder = new File("txt");
 		for (File file : txtFolder.listFiles(new TxtFileFilter())) {
-			
+
 			FinancialData financialData = new FinancialData();
 			ParserBaba parser = new ParserBaba(file.getPath());
-			
-			financialData.setCompStatement(parser.parseCompStatement());	
-			financialData.setCompanyName(parser.parseCompanyName());	
+
+			financialData.setCompStatement(parser.parseCompStatement());
+			financialData.setCompanyName(parser.parseCompanyName());
 			financialData.setAdjustedNetIncome(parser.parseAdjustedNetIncome());
 			financialData.setFinQuarter(parser.parseFinQuarter());
 			financialData.setFinYear(parser.parseFinancialYear());
 			financialData.setNetIncome(parser.parseNetIncome());
 			financialData.setRevenue(parser.parseRevenue());
-		
-			String hmKey = financialData.getCompanyName()+financialData.getFinYear()+financialData.getFinQuarter();
 
+			// Create unique key with company name, year, quarter
+			String hmKey = financialData.getCompanyName() + "_" + financialData.getFinYear() + "_" + financialData.getFinQuarter();
+
+			// Add appropriate data to financial data hashmap
 			finDataHM.put(hmKey, financialData);
-			
-			//Run the sentiment Analysis
+
+			// Run the sentiment Analysis and add to hashmap
 			SentimentAnalysisResult sentimentAnalysis = new SentimentAnalysisResult(financialData.getCompStatement());
 			sentimentAnalysis.showSentimentScore();
 			senResultHM.put(hmKey, sentimentAnalysis);
-		
-		// End of the For Loop
+
+			// End of the For Loop
 		}
-		
-		//Loop3 outputing the two HashMaps
+	}
+
+	/**
+	 * The print method prints the compensation statement to a new file named with
+	 * the key of the hashmap entry.
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public void print() throws FileNotFoundException {
 		for (String key : finDataHM.keySet()) {
-			//create folder dataset
+			// create folder dataset
 			File folder = new File("dataset");
 			folder.mkdir();
-					
+
 			// Print to file
 			File out = new File(folder, key + ".txt");
 
 			PrintWriter pw = new PrintWriter(out);
-			
+
 			String text = finDataHM.get(key).getCompStatement();
 			pw.print(text);
 			pw.flush();
 			pw.close();
-
 		}
-	
+	}
+
+	public static void main(String[] args) {
+		try {
+			Runner runner = new Runner();
+			runner.print();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
